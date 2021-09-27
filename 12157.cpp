881705b44d13d4e67b92004d62cbd21da6c1e301
Al3p4 <85536251+Al3p4@users.noreply.github.com>
2021-09-26 20:51:46 -0500

    Create 12157.cpp

---
+++ 12157.cpp
@@ -0,0 +1,32 @@
+/* 12157 - Plan Tarifario */
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int T , N ,Mile ,Juice;
+    cin>>T;
+    
+    for(int i =1; i<=T; i++){
+        cin>>N;
+        int L[N];
+        Mile =0;
+        Juice =0;
+        while(N!=0){
+            cin>>L[i]; 
+            Mile = Mile + (L[i] / 30) * 10 + 10 ;
+            Juice = Juice + (L[i] / 60) * 15 + 15;
+            N--;
+        }
+        if (Mile < Juice){
+                cout<< "Case "<<i<<": Mile "<<Mile<<endl;
+        }
+         if (Mile > Juice){
+            cout<< "Case "<<i<<": Juice "<<Juice<<endl;
+        }
+        if (Mile == Juice){
+           cout<< "Case "<<i<<": Mile Juice "<<Mile<<endl;
+        }
+    }
+    return 0;
+}
