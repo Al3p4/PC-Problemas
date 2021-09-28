25b2936e0408c3c2e367e625eff24dfd8d447846
Al3p4 <85536251+Al3p4@users.noreply.github.com>
2021-09-27 19:20:07 -0500

    Create 1124.cpp

---
+++ 1124.cpp
@@ -0,0 +1,15 @@
+// Problema 1 - P2 ->  Accepted
+//1124 - Celebrity jeopardy
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string ecuacion;
+    while(getline(cin,ecuacion)){
+    	cout<<ecuacion<<endl;
+	}
+    return 0;
+}
