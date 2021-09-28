5f4efc5a68e43f909172d870ff179456000b4d7f
Al3p4 <85536251+Al3p4@users.noreply.github.com>
2021-09-27 23:20:04 -0500

    Create 10071.cpp

---
+++ 10071.cpp
@@ -0,0 +1,15 @@
+// Problema 2  - P1 -> Accepted 
+// 10071 - Regreso a la escuela secundaria de física
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int v, t;
+   
+    while (cin >> v >> t)
+    {
+        cout << (2 * v * t) <<endl;
+    }
+}
